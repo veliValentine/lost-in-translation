899a60ea1e55fa358d2551cfc8ea3eabb3d49b9a
veliValentine <nikkevanukas1995@gmail.com>
2021-03-01 16:35:13 +0200

    feat:header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => (
+  <>
+    <div className="header">
+      <h3>Lost in translation</h3>
+      <p>Link to userpage</p>
+    </div>
+    <hr />
+  </>
+);
+
+export default Header;
