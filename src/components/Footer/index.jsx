eb966abe709b7a6586e89ba187b5650fe35bdf20
veliValentine <nikkevanukas1995@gmail.com>
2021-03-01 16:37:46 +0200

    feat: added line in Header/Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './index.css';
 
 const Footer = () => (
-  <div className="footer">
-    <a href="https://github.com/veliValentine/lost-in-translation">Source code</a>
-    <a href="https://github.com/veliValentine">Author</a>
-  </div>
+  <>
+    <hr />
+    <div className="footer">
+      <a href="https://github.com/veliValentine/lost-in-translation">Source code</a>
+      <a href="https://github.com/veliValentine">Author</a>
+    </div>
+  </>
 );
 
 export default Footer;
