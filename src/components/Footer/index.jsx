f8005ff5670092ac44b88423badfcebb20b65f4e
veliValentine <nikkevanukas1995@gmail.com>
2021-03-01 16:28:08 +0200

    feat:footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () => (
+  <div className="footer">
+    <a href="https://github.com/veliValentine/lost-in-translation">Source code</a>
+    <a href="https://github.com/veliValentine">Author</a>
+  </div>
+);
+
+export default Footer;
