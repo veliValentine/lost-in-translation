c3df3999b7d02200d57671d931eccc09f6015486
veliValentine <nikkevanukas1995@gmail.com>
2021-03-04 11:55:58 +0200

    feat: check authenticated user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import {
-  Redirect, Route, Switch, useHistory,
-} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
 
 import useUser from './hooks/useUser';
 import useTranslations from './hooks/useTranslations';
@@ -11,30 +9,30 @@ import Header from './components/Header';
 import LoginPage from './components/LoginPage';
 import ProfilePage from './components/ProfilePage';
 import TranslationPage from './components/TranslationPage';
-import authenticated from './utils/authenticated';
 
 const App = () => {
-  const [user, updateUser, clearUser] = useUser();
+  const [user, updateUser] = useUser();
   const [translations, addTranslation, clearTranslations] = useTranslations(user);
   const history = useHistory();
 
+  useEffect(() => {
+    console.log('effect');
+  }, []);
+
   const login = (username = null) => {
+    clearTranslations();
     updateUser(username);
     history.push('/');
   };
-  authenticated()();
   const logout = () => {
-    clearUser();
-    clearTranslations();
+    login();
   };
-
   return (
     <div>
-      {!user && <Redirect to="/login" />}
       <Header user={user} />
       <Switch>
         <Route path="/login">
-          <LoginPage login={login} user={user} />
+          <LoginPage login={login} user={user} logout={logout} />
         </Route>
         <Route path="/user">
           <ProfilePage
