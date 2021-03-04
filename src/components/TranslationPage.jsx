73a819ca1a7a8fd3bbd62a42ea8b10751996d4c5
veliValentine <nikkevanukas1995@gmail.com>
2021-03-04 11:19:32 +0200

    feat: translation to sign images

--- src/components/TranslationPage.jsx
+++ src/components/TranslationPage.jsx
@@ -1,35 +1,42 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useState } from 'react';
 import Form from './Form';
+import getSignImages from '../utils/getSignImages';
+import parseInput from '../utils';
 
-const Translation = ({ input = '' }) => {
-  if (input === '') {
-    return null;
-  }
-  const translatedString = input.split('')
-    .map((letter) => letter); // TODO convert letter to correct sign image
-  return (
-    <div>
-      <p>Translation:</p>
-      {translatedString}
-    </div>
-  );
-};
+const signs = getSignImages();
 
 const TranslationPage = ({ addTranslation }) => {
   const [translation, setTranslation] = useState('');
-
-  const submitTranslation = (inputTranslation) => {
+  const submitTranslation = (input) => {
+    const inputTranslation = parseInput(input);
     if (inputTranslation !== '') {
       setTranslation(inputTranslation);
       addTranslation(inputTranslation);
     }
   };
-
   return (
     <div className="translation-page">
       <h1>Translation page</h1>
       <Form onSubmit={submitTranslation} placeholder="Translation" buttonText="Translate" />
       <Translation input={translation} />
+    </div>
+  );
+};
+
+const Translation = ({ input = '' }) => {
+  if (input === '') {
+    return null;
+  }
+  const translate = parseInput(input);
+  const translatedString = translate.split('')
+    .map((letter, index) => <img key={index} src={signs[letter]} alt="poop" />);
+  return (
+    <div>
+      <p>{`${translate} translated to`}</p>
+      <div className="translation">
+        {translatedString}
+      </div>
     </div>
   );
 };
