da8fa3870258a457aeba59c7151c1a4e596daff1
veliValentine <nikkevanukas1995@gmail.com>
2021-03-05 14:06:39 +0200

    feat(style): styles to login, user and error page

--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './errorPage.css';
+
 const ErrorPage = () => (
   <div className="error-page">
     <h3>There was an unexpected error</h3>
